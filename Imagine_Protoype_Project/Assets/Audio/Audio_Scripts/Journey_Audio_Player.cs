42f33c93a2e2bb2e25f446ef2ff222ccb580f07d
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2018-10-21 22:40:19 +0100

    Commit to me daddy

--- Imagine_Protoype_Project/Assets/Audio/Audio_Scripts/Journey_Audio_Player.cs
+++ Imagine_Protoype_Project/Assets/Audio/Audio_Scripts/Journey_Audio_Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 [RequireComponent(typeof(AudioManager))]
@@ -74,6 +75,13 @@ public bool playing;
           }
 
 
+     }
+
+
+     public void ReturnToMap() {
+
+          SceneManager.LoadScene("Map_Scene");
+          
      }
 
 
