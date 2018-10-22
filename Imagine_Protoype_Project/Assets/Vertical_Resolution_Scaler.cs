4d16fec2a9336874d6ef460a6dfbdff2ed3af434
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2018-10-22 16:04:31 +0100

    WindowScaler

---
+++ Imagine_Protoype_Project/Assets/Vertical_Resolution_Scaler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vertical_Resolution_Scaler : MonoBehaviour {
+
+	  private void Awake()
+         {
+             //Set screen size for Standalone
+     #if UNITY_STANDALONE
+             Screen.SetResolution(564, 960, false);
+             Screen.fullScreen = false;
+     #endif
+         }
+}
