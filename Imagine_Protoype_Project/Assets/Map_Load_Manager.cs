484559b166b83a82c741c8e1fc4d89b4d59b5b42
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2018-10-22 22:06:37 +0100

    MapLoading

---
+++ Imagine_Protoype_Project/Assets/Map_Load_Manager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using UnityEngine.SceneManagement;
+
+
+public class Map_Load_Manager : MonoBehaviour {
+
+
+
+
+
+
+
+	void OnEnable() {
+		//Tell our 'OnLevelFinishedLoading' function to start listening for a scene change as soon as this script is enabled.
+		SceneManager.sceneLoaded += OnLevelFinishedLoading;
+	}
+
+	void OnDisable() {
+		//Tell our 'OnLevelFinishedLoading' function to stop listening for a scene change as soon as this script is disabled. Remember to always have an unsubscription for every delegate you subscribe to!
+		SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+	}
+
+	void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode) {
+		Debug.Log("Level Loaded");
+		Debug.Log(scene.name);
+		Debug.Log(mode);
+		
+		
+		CheckMap();
+	}
+	
+	
+	
+	
+	
+	
+	private void CheckMap() {
+		
+		Debug.Log("Start triggered");
+		
+		DontDestroyOnLoad(gameObject);
+		
+
+		string currentSceneName = SceneManager.GetActiveScene().name;
+
+		if (currentSceneName == "Map_Scene") {
+
+			
+			transform.GetChild(0).gameObject.SetActive(true);
+			
+			
+			
+		} else {
+
+			transform.GetChild(0).gameObject.SetActive(false);
+			
+			
+		}
+
+
+
+	}
+
+	
+	
+}
