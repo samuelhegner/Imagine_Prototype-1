31487910009ca107d9f2182031f7fd9601060353
samuelhegner <C16439262@mydit.ie>
2018-10-25 16:36:26 +0100

    Trail Rendering (Samuel Hegner)

---
+++ Imagine_Protoype_Project/Assets/CreateTrail.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateTrail : MonoBehaviour {
+
+    public int length;
+    public float distanceBetweenPoints = 10f;
+    public float rangeX;
+
+    LineRenderer rend;
+
+    float randomXOffset;
+
+    Vector3[] Points;
+
+    private void Awake()
+    {
+        rend = GetComponent<LineRenderer>();
+        Vector3 [] TempPoints = new Vector3[length];
+        for (int i = 0; i < TempPoints.Length; i++) {
+
+            float lastX = transform.position.x;
+            float firstY = transform.position.y;
+
+            if (i == 0)
+            {
+                TempPoints[i] = transform.position;
+            }
+            else {
+                randomXOffset = Random.Range(-(rangeX), (rangeX));
+                TempPoints[i] = new Vector3(lastX + randomXOffset, firstY + distanceBetweenPoints * i, 0);
+                lastX = TempPoints[i].x;
+            }
+        }
+
+        Points = Game_Manager.MakeSmoothCurve(TempPoints, 5f);
+
+        rend.positionCount = Points.Length;
+        rend.SetPositions(Points);
+        rend.startWidth = 5f;
+        rend.endWidth = 5f;
+    }
+}
