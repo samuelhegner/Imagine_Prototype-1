82445dd2972deff390df73467f351fefbdf80e5c
samuelhegner <C16439262@mydit.ie>
2018-10-25 15:07:32 +0100

    Star Prefab

---
+++ Imagine_Protoype_Project/Assets/Star_Collision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star_Collision : MonoBehaviour {
+    public Animator anim;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == "Player") {
+            int ran = Random.Range(0, 2);
+            print(ran);
+
+            if (ran < 1f)
+            {
+                anim.SetTrigger("CollisionRight");
+            }
+            else {
+                anim.SetTrigger("CollisionLeft");
+            }
+        }
+    }
+}
