4ded0fbabee035639f52fa1b621dcd7479e18101
samuelhegner <C16439262@mydit.ie>
2018-11-01 20:52:47 +0000

    Hell yeah

--- Imagine_Protoype_Project/Assets/Adjust_Size.cs
+++ Imagine_Protoype_Project/Assets/Adjust_Size.cs
@@ -9,18 +9,23 @@ public class Adjust_Size : MonoBehaviour {
 
 	public float height;
     public float buffer;
+	int lastSeg;
 
 	// Use this for initialization
 	void Start () {
 		parent = transform.parent.gameObject;
 		col = GetComponent<BoxCollider2D>();
 
+		lastSeg = parent.GetComponent<CreateTrail>().smoothness * parent.GetComponent<CreateTrail>().length;
 		
 
 		if(parent.transform.childCount == 1){
 			StartCoroutine(GetHeight());
-		}else{
+		}else if(parent.transform.childCount < lastSeg){
 			height = Vector2.Distance(parent.transform.GetChild(0).transform.position, parent.transform.GetChild(1).transform.position) + buffer;
+		}else if(parent.transform.childCount == lastSeg){
+			height = parent.transform.GetChild(lastSeg-2).GetComponent<Adjust_Size>().height;
+			transform.rotation = parent.transform.GetChild(lastSeg-2).transform.rotation;
 		}
 
 		float width = parent.GetComponent<LineRenderer>().startWidth;
--- Imagine_Protoype_Project/Assets/CreateTrail.cs
+++ Imagine_Protoype_Project/Assets/CreateTrail.cs
@@ -8,7 +8,7 @@ public class CreateTrail : MonoBehaviour
     public int length;
     public float distanceBetweenPoints = 10f;
     public float rangeX;
-    public float smoothness;
+    public int smoothness;
 
     float lineWidth;
 
@@ -19,7 +19,6 @@ public class CreateTrail : MonoBehaviour
     Vector3[] Points;
 
     public GameObject box;
-    Quaternion lastRot;
 
     private void Awake()
     {
@@ -41,7 +40,7 @@ public class CreateTrail : MonoBehaviour
             }
         }
 
-        Points = Game_Manager.MakeSmoothCurve(TempPoints, smoothness);
+        Points = Game_Manager.MakeSmoothCurve(TempPoints, (float)smoothness);
 
         for (int i = 0; i < Points.Length -1; i++){
             GameObject boxTemp = Instantiate(box, Points[i], transform.rotation);
@@ -52,18 +51,17 @@ public class CreateTrail : MonoBehaviour
         {
             
             
-            if(i < transform.childCount-2){
+            if(i < transform.childCount){
                 Vector3 toVector = Points[i + 1] - Points[i];
                 float angle = Mathf.Atan2(toVector.y, toVector.x) * Mathf.Rad2Deg;
                 Quaternion NewRot = Quaternion.AngleAxis(angle, Vector3.forward);
-                lastRot = NewRot;
-                transform.GetChild(i).transform.rotation = NewRot;
-            }else{
-                transform.GetChild(i).transform.rotation = lastRot;
-            }
 
+                transform.GetChild(i).transform.rotation = NewRot;
+            }
         }
 
+        transform.GetChild(transform.childCount-1).transform.rotation = transform.GetChild(transform.childCount-2).transform.rotation;
+        
 
         rend.positionCount = Points.Length;
         rend.SetPositions(Points);
@@ -71,49 +69,5 @@ public class CreateTrail : MonoBehaviour
         rend.endWidth = 5f;
 
         lineWidth = rend.startWidth;
-
-
-
-        /*Vector2[] TwoDeePoints = new Vector2[Points.Length];
-        for (int i = 0; i < TwoDeePoints.Length; i++) {
-            Vector3[] TwoDeePointsTemp = new Vector3[Points.Length];
-            TwoDeePointsTemp[i] = transform.InverseTransformPoint(Points[i].x, Points[i].y, 0);
-            TwoDeePoints[i] = new Vector2(TwoDeePointsTemp[i].x, TwoDeePointsTemp[i].y);
-        }
-
-        Vector2[] RightArray = new Vector2[Points.Length]; ;
-        Vector2[] LeftArray = new Vector2[Points.Length];
-
-        float angle;
-        float xOff;
-        float yOff;
-
-        for (int i = 0; i < TwoDeePoints.Length; i++) {
-
-            xOff = 0;
-            yOff = 0;
-            
-            if (i < TwoDeePoints.Length - 1) {
-                angle = Vector2.SignedAngle(Vector3.up, (TwoDeePoints[i + 1] - TwoDeePoints[i]));
-
-                float percentAngle = 90 / angle;
-                yOff = (rend.startWidth / 2) / percentAngle;
-                xOff = (rend.startWidth / 2) - yOff;
-
-                print("yOff= " + yOff);
-                print("xOff= " + xOff);
-            }
-
-            
-
-            LeftArray[i] = new Vector2(TwoDeePoints[i].x, TwoDeePoints[i].y);
-            RightArray[i] = new Vector2(TwoDeePoints[i].x + rend.startWidth / 2, TwoDeePoints[i].y);
-        }
-
-        EdgeCollider2D edgeRight = gameObject.AddComponent<EdgeCollider2D>();
-        EdgeCollider2D edgeLeft = gameObject.AddComponent<EdgeCollider2D>();
-
-        edgeRight.points = RightArray;
-        edgeLeft.points = LeftArray;*/
     }
 }
