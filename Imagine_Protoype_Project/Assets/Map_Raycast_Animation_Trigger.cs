71a8dafa1df791f6f86a4a909b6dd09ec47d246e
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2018-11-05 22:01:55 +0000

    woops

---
+++ Imagine_Protoype_Project/Assets/Map_Raycast_Animation_Trigger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map_Raycast_Animation_Trigger : MonoBehaviour {
+
+	private void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+
+AttemptTrigger();
+			
+			
+		}
+
+		if (Input.touchCount == 1) {
+
+			if (Input.GetTouch(0).phase == TouchPhase.Began) {
+
+				AttemptTrigger();
+				
+			}
+
+		}
+
+	}
+
+
+	void AttemptTrigger() {
+
+		RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+		if (hit.collider != null) {
+
+			if (hit.collider.gameObject.CompareTag("Map_Interactable")){
+
+
+				hit.collider.gameObject.GetComponent<Animator>().SetTrigger("Animate");
+				
+				
+			}
+
+
+		}
+
+
+
+	}
+
+
+
+}
