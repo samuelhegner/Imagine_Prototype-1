ea3f3c44fb1fb8ef77653794dcfed57f8a4addcb
samuelhegner <C16439262@mydit.ie>
2018-11-06 14:40:00 +0000

    Galaxy Start

---
+++ Imagine_Protoype_Project/Assets/Galaxy_Start.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Galaxy_Start : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag == "Player"){
+			CreateUniverse();
+		}
+	}
+
+	void CreateUniverse(){
+		for(int i = 0; i < transform.childCount; i++){
+			transform.GetChild(i);
+		}
+	}
+}
