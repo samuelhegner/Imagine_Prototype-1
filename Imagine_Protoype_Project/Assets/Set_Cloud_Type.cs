861518161e6d25634a1925a667d753be5141e77d
samuelhegner <C16439262@mydit.ie>
2018-11-08 18:01:44 +0000

    Stuff

---
+++ Imagine_Protoype_Project/Assets/Set_Cloud_Type.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Set_Cloud_Type : MonoBehaviour {
+
+	public GameObject[]pref;
+	void Awake () {
+		int ran = Random.Range(0, 3);
+		Instantiate(pref[ran], transform.position, transform.rotation);
+		Destroy(gameObject);
+	}
+
+}
