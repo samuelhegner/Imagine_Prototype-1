d9763fb5ded1e7a35f9a0a9e0e6b1b59a58d64e2
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2018-11-12 14:47:16 +0000

    changes

---
+++ Imagine_Protoype_Project/Assets/Map_Layer_manager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map_Layer_manager : MonoBehaviour {
+
+	//public int SL;
+	
+	
+	private GameObject[] GameObjectsToSort;
+
+	public layerSortOnY[] LSOY;
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+		yield return null;
+
+		//GameObjectsToSort = FindGameObjectsWithLayer(SL);
+
+		//GameObjectsToSort = FindGameObjectsWithObject();
+
+
+		LSOY = FindObjectsOfType<layerSortOnY>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		foreach (layerSortOnY LS in LSOY) {
+
+			try {
+				//GO.GetComponentInChildren<SpriteRenderer>().sortingOrder = Mathf.RoundToInt(transform.position.y * 100f) * -1;
+				
+				int idealOrder = Mathf.RoundToInt(LS.transform.position.y * 100f) * -1;
+				
+				LS.ReorderSprites(idealOrder);
+				
+				
+			} catch {
+
+				
+				
+			}
+		}
+
+
+
+
+	}
+
+
+	GameObject[] FindGameObjectsWithLayer(int layer){
+		Renderer[] goArray = FindObjectsOfType<Renderer>();
+		var goList  = new List < GameObject > ();
+
+		for (var i = 0; i < goArray.Length; i++) {
+			if (goArray[i].sortingLayerID == layer) {
+				goList.Add(goArray[i].transform.root.gameObject);
+			}
+		}
+
+		if (goList.Count == 0) {
+			return null;
+		}
+
+		return goList.ToArray();
+	}
+
+
+	GameObject[] FindGameObjectsWithObject() {
+
+		layerSortOnY[] temp = FindObjectsOfType<layerSortOnY>();
+
+		GameObject[] tempGO = new GameObject[temp.Length];
+
+		for (int i = 0; i < temp.Length; i++) {
+
+
+			tempGO[i] = temp[i].gameObject;
+
+
+		}
+		
+	
+
+		return tempGO;
+
+
+	}
+
+
+
+
+
+}
---
+++ Imagine_Protoype_Project/Assets/layerSortOnY.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class layerSortOnY : MonoBehaviour {
+
+	public SpriteRenderer[] SpritesToOrder;
+
+
+
+	public void ReorderSprites(int order) {
+
+		for (int i = 0; i < SpritesToOrder.Length; i++) {
+
+			SpritesToOrder[i].sortingOrder = order - i;
+
+
+
+		}
+
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
