b93e3bdf571ea58e070f9f72de344baa6d8ab5ca
gazor117 <oreillygary7@gmail.com>
2018-11-12 22:17:00 +0000

    Cleaned Project Folder

---
+++ Imagine_Protoype_Project/Assets/Scripts/Cosmos_Scripts/Galaxy_Child.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Galaxy_Child : MonoBehaviour {
+    public int index;
+}
---
+++ Imagine_Protoype_Project/Assets/Scripts/Cosmos_Scripts/Parallax.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+    public Transform[] backgrounds;
+
+    float[] parallaxScales;
+
+    public float smoothing;
+
+    Transform cam;
+
+    Vector3 previousCamPos;
+
+    void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
+    void Start()
+    {
+        previousCamPos = cam.position;
+
+        parallaxScales = new float[backgrounds.Length];
+
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z * -1;
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
+
+            float backgroundTargetPositionX = backgrounds[i].position.x + parallax;
+
+            Vector3 backgroundTargetPosition = new Vector3(backgroundTargetPositionX, backgrounds[i].position.y, backgrounds[i].position.z);
+
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPosition, smoothing * Time.deltaTime);
+        }
+
+        previousCamPos = cam.position;
+    }
+}
