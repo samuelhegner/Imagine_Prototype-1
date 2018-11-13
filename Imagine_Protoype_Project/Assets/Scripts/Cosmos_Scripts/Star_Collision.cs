0a7ab417cd29f4e58452c0f01a931efe7727656f
gazor117 <oreillygary7@gmail.com>
2018-11-13 14:53:17 +0000

    Star Activating

--- Imagine_Protoype_Project/Assets/Scripts/Cosmos_Scripts/Star_Collision.cs
+++ Imagine_Protoype_Project/Assets/Scripts/Cosmos_Scripts/Star_Collision.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class Star_Collision : MonoBehaviour {
     public Animator anim;
-	// Use this for initialization
+    private bool starHit = false;
+    public GameObject starEmitterParent;
+
+    // Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (starHit == true)
+		{
+            starEmitterParent.SetActive(true);
+
+		}
 	}
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.transform.tag == "Player") {
+        if (other.transform.tag == "Player")
+        {
+            starHit = true;
+            
             int ran = Random.Range(0, 2);
 
             if (ran < 1f)
