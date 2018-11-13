6c6eb9c0092c965919e4ff2cfd88fb9588c2dce9
samuelhegner <C16439262@mydit.ie>
2018-11-13 20:01:32 +0000

    Cloud Fuctionality done (Samuel Hegner)

--- Imagine_Protoype_Project/Assets/Move_Cloods.cs
+++ Imagine_Protoype_Project/Assets/Move_Cloods.cs
@@ -7,13 +7,27 @@ public class Move_Cloods : MonoBehaviour {
 	float countdown;
 	public float maxCountdown;
 	public float speed;
+	float a;
+
+	float dist;
+	public float maxDist;
 
 	public bool x;
 
 	Vector3 idlePos;
 	Vector3 movePos;
 
+	GameObject player;
+
+	SpriteRenderer rend;
+
+	void Awake(){
+		player = GameObject.FindGameObjectWithTag("Player");
+		rend = transform.GetChild(0).GetComponent<SpriteRenderer>();
+	}
+
 	void Start () {
+		a = 0;
 		idlePos = transform.position;
 		if(x){
 			movePos = new Vector3(transform.parent.transform.position.x, transform.position.y, 0);
@@ -23,12 +37,35 @@ public class Move_Cloods : MonoBehaviour {
 	}
 	
 	void Update () {
+		dist = Vector2.Distance(player.transform.position, transform.position);
+		Color col = new Color(1, 1, 1, a);
+
+		if(dist < maxDist){
+			if(a < 1)
+			a += Time.deltaTime;
+		}else{
+			if(a > 0)
+			a -= Time.deltaTime;
+		}
+
+		if(x){
+			idlePos.y = player.transform.position.y;
+			movePos.y = player.transform.position.y;
+		}else{
+			idlePos.x = player.transform.position.x;
+			movePos.x = player.transform.position.x;
+		}
+
+
+
 		if(countdown > 0){
 			countdown -= Time.deltaTime;
 			transform.position = Vector3.Lerp(transform.position, movePos, Time.deltaTime * speed);
 		}else{
 			transform.position = Vector3.Lerp(transform.position, idlePos, Time.deltaTime * speed/2f);
 		}
+
+		rend.color = col;
 	}
 
 	public void ResetTimer(){
