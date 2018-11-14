f0cdc2ae2b69ef66042f2ccee6e4b7ef7f0e5d87
Samuel Hegner <C16439262@mydit.ie>
2018-11-14 12:23:40 +0000

    New Ui Prefab Map (Samuel Hegner)

---
+++ Imagine_Protoype_Project/Assets/Select_Site.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Select_Site : MonoBehaviour {
+
+    bool showing;
+
+    public GameObject popUp;
+
+    public void SwitchPopUp(){
+        showing = !showing;
+        popUp.SetActive(showing);
+    }
+
+    public void TurnOffPopUp(){
+        showing = false;
+        popUp.SetActive(showing);
+    }
+}
