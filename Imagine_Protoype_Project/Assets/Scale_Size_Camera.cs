33fbee3b7d35f441bfba4e199c1e5b9369c4b310
samuelhegner <C16439262@mydit.ie>
2018-11-14 19:28:04 +0000

    Map Scene Full UI Overhaul

---
+++ Imagine_Protoype_Project/Assets/Scale_Size_Camera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scale_Size_Camera : MonoBehaviour {
+
+	public Pinch_Zoom_Camera pinch;
+
+	float minCamSize;
+	float maxCamSize;
+	public float minScale = 1;
+	public float maxScale = 10;
+
+	Vector3 newScale;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		minCamSize = pinch.minZoom;
+		maxCamSize = pinch.maxZoom;
+
+		float scale = Map(Camera.main.orthographicSize, minCamSize, maxCamSize, minScale, maxScale);
+
+		newScale = new Vector3( scale, scale, 0);
+
+		transform.localScale = newScale;
+	}
+
+	float Map(float a, float b, float c, float d, float e)
+    {
+        float cb = c - b;
+        float de = e - d;
+        float howFar = (a - b) / cb;
+        return d + howFar * de;
+    }
+}
