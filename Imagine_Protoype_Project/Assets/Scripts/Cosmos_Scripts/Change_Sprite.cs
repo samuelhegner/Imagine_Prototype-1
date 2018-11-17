225e60375b23c6a39c559fce41f2bc4d715c1d6b
gazor117 <oreillygary7@gmail.com>
2018-11-17 15:03:31 +0000

    Cosmos Layout Changed

--- Imagine_Protoype_Project/Assets/Scripts/Cosmos_Scripts/Change_Sprite.cs
+++ Imagine_Protoype_Project/Assets/Scripts/Cosmos_Scripts/Change_Sprite.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Change_Sprite : MonoBehaviour {
 
-	public Sprite[] sprites = new Sprite[3];
+	//public Sprite[] sprites = new Sprite[3];
+	public GameObject[] prefab;
 	// Use this for initialization
 	void Awake () {
-		int ran = Random.Range(0, sprites.Length);
+		int ran = Random.Range(0, prefab.Length);
 
-		GetComponent<SpriteRenderer>().sprite = sprites[ran];
+		/*GetComponent<SpriteRenderer>().sprite = sprites[ran];*/
 		PolygonCollider2D col = gameObject.AddComponent<PolygonCollider2D>();
 		col.isTrigger = true;
+
+		Instantiate(prefab[ran], transform.position, transform.rotation);
 	}
 }
