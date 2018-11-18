613d93181cfbb042918ab608354cbc10e50628a6
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2018-11-18 20:42:13 +0000

    PC bool

--- Imagine_Protoype_Project/Assets/Scripts/Forest_Minigame_Scripts/Forest_Touch_Manager.cs
+++ Imagine_Protoype_Project/Assets/Scripts/Forest_Minigame_Scripts/Forest_Touch_Manager.cs
@@ -23,6 +23,8 @@ public class Forest_Touch_Manager : MonoBehaviour {
 
      public Transform Wind;
 
+     public bool PC = true; 
+
 
      // Use this for initialization
      private void Start() {
@@ -37,104 +39,114 @@ public class Forest_Touch_Manager : MonoBehaviour {
      // Update is called once per frame
      private void Update() {
 
+          if (!PC) {
+               //for (int i = 0; i < Input.touchCount; ++i) {
+               if (Input.touchCount > 0) {
 
-          //for (int i = 0; i < Input.touchCount; ++i) {
-          if (Input.touchCount > 0) {
+                    if (Input.GetTouch(0).phase == TouchPhase.Began) {
 
-               if (Input.GetTouch(0).phase == TouchPhase.Began) {
+                         Vector2 TrailPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
 
-                    Vector2 TrailPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                         _thisTrail = Instantiate(TrailPrefab, TrailPos, Quaternion.identity);
 
-                    _thisTrail = Instantiate(TrailPrefab, TrailPos, Quaternion.identity);
+                         _trailRenderer = _thisTrail.GetComponent<TrailRenderer>();
 
-                    _trailRenderer = _thisTrail.GetComponent<TrailRenderer>();
+                         // EZCameraShake.CameraShaker.Instance.ShakeOnce(1f, 1f, 0.2f, 2f);
 
-                   // EZCameraShake.CameraShaker.Instance.ShakeOnce(1f, 1f, 0.2f, 2f);
+                         EZCameraShake.CameraShaker.Instance.StartShake(0.5f, 0.5f, 0.2f);
 
-                    EZCameraShake.CameraShaker.Instance.StartShake(0.5f, 0.5f, 0.2f);
+                         //AudioManager.Master.Play("Wind");
 
-                    //AudioManager.Master.Play("Wind");
+                         //		Ray mRay = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
 
-                    //		Ray mRay = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+                         //float rayDistance;
 
-                    //float rayDistance;
+                         //			if (_objPlane.Raycast(mRay, out rayDistance)) {
 
-                    //			if (_objPlane.Raycast(mRay, out rayDistance)) {
+                         //		_startPos = mRay.GetPoint(rayDistance);
 
-                    //		_startPos = mRay.GetPoint(rayDistance);
-
-                    //	}
-
-
-                    //clone = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
-
-                    //	lookPosition = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
+                         //	}
 
 
-                    //WindTrans.LookAt(lookPosition);
+                         //clone = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
 
-               } else if (Input.GetTouch(0).phase == TouchPhase.Moved) {
-
-                    //	Ray mRay = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+                         //	lookPosition = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
 
 
-                    //		float rayDistance;
+                         //WindTrans.LookAt(lookPosition);
 
-                    //		if (_objPlane.Raycast(mRay, out rayDistance)) {
+                    } else if (Input.GetTouch(0).phase == TouchPhase.Moved) {
 
-                    Vector2 TrailPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-
-                    _thisTrail.transform.position = TrailPos; //mRay.GetPoint(rayDistance);
-
-                    //}
+                         //	Ray mRay = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
 
 
-               } else if (Input.GetTouch(0).phase == TouchPhase.Ended) {
+                         //		float rayDistance;
 
-                    //Destroy(_thisTrail, );
+                         //		if (_objPlane.Raycast(mRay, out rayDistance)) {
+
+                         Vector2 TrailPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+
+                         _thisTrail.transform.position = TrailPos; //mRay.GetPoint(rayDistance);
+
+                         //}
+
+
+                    } else if (Input.GetTouch(0).phase == TouchPhase.Ended) {
+
+                         //Destroy(_thisTrail, );
+
+                         _thisTrail.GetComponent<TrailRenderer>().time = 1f;
+                         Destroy(_thisTrail, 1f);
+
+                         EZCameraShake.CameraShaker.Instance.ShakeInstances[0].StartFadeOut(0.4f);
+
+
+                    }
+
+
+
+
+               }
+
+
+          }
+
+
+          if (PC) {
+               if (Input.GetMouseButton(0)) {
+
+                    if (Input.GetMouseButtonDown(0)) {
+
+                         Vector2 TrailPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                         _thisTrail = Instantiate(TrailPrefab, TrailPos, Quaternion.identity);
+
+                         _trailRenderer = _thisTrail.GetComponent<TrailRenderer>();
+
+                         EZCameraShake.CameraShaker.Instance.StartShake(0.5f, 0.5f, 0.2f); //ShakeOnce(1f, 1f, 0.2f, 2f);
+
+
+                    } else {
+
+                         Vector2 TrailPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                         _thisTrail.transform.position = TrailPos;
+
+
+                    }
+
+
+
+
+               } else if (Input.GetMouseButtonUp(0)) {
 
                     _thisTrail.GetComponent<TrailRenderer>().time = 1f;
                     Destroy(_thisTrail, 1f);
 
-                    EZCameraShake.CameraShaker.Instance.ShakeInstances[0].StartFadeOut(0.4f);
-                    
+                    EZCameraShake.CameraShaker.Instance.ShakeInstances[0].StartFadeOut(0.4f); //Shake(EZCameraShake.CameraShakePresets.Earthquake);
+
 
                }
-
-
-          } else if (Input.GetMouseButton(0)){
-
-               if (Input.GetMouseButtonDown(0)) {
-
-                    Vector2 TrailPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-                    _thisTrail = Instantiate(TrailPrefab, TrailPos, Quaternion.identity);
-
-                    _trailRenderer = _thisTrail.GetComponent<TrailRenderer>();
-
-                    EZCameraShake.CameraShaker.Instance.StartShake(0.5f, 0.5f, 0.2f); //ShakeOnce(1f, 1f, 0.2f, 2f);
-
-
-               } else {
-
-                    Vector2 TrailPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-                    _thisTrail.transform.position = TrailPos;
-                    
-                    
-               }
-
-
-
-
-          } else if (Input.GetMouseButtonUp(0)) {
-
-               _thisTrail.GetComponent<TrailRenderer>().time = 1f;
-               Destroy(_thisTrail, 1f);
-
-               EZCameraShake.CameraShaker.Instance.ShakeInstances[0].StartFadeOut(0.4f);//Shake(EZCameraShake.CameraShakePresets.Earthquake);
-
-
           }
 
 
